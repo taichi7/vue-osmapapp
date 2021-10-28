124047c936b306e846f24bfda8bb29c687e025be
taichi7 <93295480+taichi7@users.noreply.github.com>
2021-10-28 11:20:46 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
+    <nav class="navbar has-background-info">
+      <div class="navbar-brand">
+        <a class="navbar-item has-text-white" href="https://i-doctor.sakura.ne.jp/font/?p=40818"> Vue OpenStreetMap Distance </a>
+      </div>
+    </nav>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Map</router-link>
+      <span> | </span>
+      <router-link to="/about">Distance</router-link>
     </div>
     <router-view/>
   </div>
@@ -10,22 +16,19 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
-  padding: 30px;
+  padding: 2px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
