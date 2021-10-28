1dc7931b365d5d3c497f3c05150a8b8016308a6a
taichi7 <93295480+taichi7@users.noreply.github.com>
2021-10-28 11:13:13 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="about">
-    <h1>現在地</h1>
-    <p>緯度：{{latitude}}</p>
-    <p>経度：{{longitude}}</p>
+    <section class="section">
+      <div class="container">
+        <article class="message is-primary">
+          <div class="message-header">
+            <p>現在地</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            <p>緯度：{{latitude | round}} 　 経度：{{longitude | round}}</p>
+          </div>
+        </article>
 
-    <h2>1分前</h2>
-    <p>緯度：{{lat2}}</p>
-    <p>経度：{{lng1}}</p>
+        <article class="message is-info">
+          <div class="message-header">
+            <p>1分前</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            <p>緯度：{{lat1 | round}} 　 経度：{{lng1 | round}}</p>
+          </div>
+        </article>
 
-    <h2>歩いた距離</h2>
-    <p>1分間で歩いた距離：{{d}} km</p>
-    <p>合計：{{length}} km</p>
+        <article class="message is-dark">
+          <div class="message-header">
+            <p>歩いた距離</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            <p>1分間で歩いた距離：{{d | round}} km</p>
+            <p>合計：{{length | round}} km</p>
+          </div>
+        </article>
+      </div>
+    </section>
+
+    <footer class="footer">
+      <a href="https://i-doctor.sakura.ne.jp/font/?p=408184">WordPressでフリーオリジナルフォント2</a>
+    </footer>
+
   </div>
 </template>
 
@@ -26,10 +54,13 @@ export default {
       lng2: 0,
     
       d: 0,
-      length: 0
+      length: 0,
     }
   },
   mounted() {
+    if (localStorage.length) {
+      this.length = localStorage.length
+    }
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         function(position){
@@ -42,27 +73,24 @@ export default {
           //10秒毎にcalcDistance関数を実行する
           setInterval(function () {
             console.log('hi');
-            /*
-            navigator.geolocation.getCurrentPosition(
-              function(position){
-                self.lat2 = position.coords.latitude;
-                self.lng2 = position.coords.longitude;
-              })
-              */
-              calcDistance();
-          }, 20000);
+            calcDistance();
+          }, 5000);
           function calcDistance(){
             navigator.geolocation.getCurrentPosition(
               function(position) { 
                 console.log('ho');
                 self.lat2 = position.coords.latitude;
                 self.lng2 = position.coords.longitude;
-                //self.lat1 = self.lat2
-                //self.lng1 = self.lng2
-                self.d = distance(self.lat1, self.lng1, self.lat2, self.lng2)
+                if (self.lat1) {
+                  self.d = distance(self.lat1, self.lng1, self.lat2, self.lng2)
+                } else {
+                  self.lat1 = self.lat2
+                  self.lng1 = self.lng2
+                }
                 self.length = self.length + self.d
-                
                 console.log(self.d);
+                console.log(self.length);
+                localStorage.setItem('length', self.length)
                 self.lat1 = self.lat2
                 self.lng1 = self.lng2
               }
@@ -85,6 +113,12 @@ export default {
     } else {
       // エラー処理を書く
       console.log("error");
+    }
+  },
+  filters: {
+    // 小数点以下を第3位に丸めるフィルタ
+    round: function (val) {
+      return Math.round(val * 1000) / 1000
     }
   }
 }
